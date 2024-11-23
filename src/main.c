9adac3e3224672d2a53ceaeef997c32ecc2bee6c
HormiDev <ivanddiegomegia@gmail.com>
2024-11-23 03:45:52 +0100

    version main simplifacado para dos comandos (sin bonus)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:29:55 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/11/21 22:08:08 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/11/23 03:42:24 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-
+/*
 void	ft_executions(t_pipex *pipex, char **envp)
 {
 	int		fd[2];
@@ -114,6 +114,73 @@ void	ft_executions(t_pipex *pipex, char **envp)
 		i--;
 	}
 	close(infile_fd);
+	if (WIFEXITED(last_exit_cmd_status))
+	{
+		ft_alloc_lst(0, 0);
+		exit(WEXITSTATUS(last_exit_cmd_status));
+	}
+}*/
+
+void	ft_executions(t_pipex *pipex, char **envp)
+{
+	int		infile_fd;
+	int		outfile_fd;
+	pid_t	pid1;
+	pid_t	pid2;
+	int		last_exit_cmd_status;
+	
+	infile_fd = open(pipex->infile, O_RDONLY);
+	outfile_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	pipe(pipex->pipe_fd);
+	pid1 = fork();
+	if (pid1 < 0)
+	{
+		ft_putstr_fd("Error al crear el proceso hijo\n", 2);
+		exit(1);
+	}
+	if (pid1 == 0)
+	{
+		if (infile_fd < 0)
+		{
+			ft_putstr_fd("Error al abrir el archivo de entrada\n", 2);
+			exit(127);
+		}
+		dup2(infile_fd, 0);
+		dup2(pipex->pipe_fd[1], 1);
+		close(infile_fd);
+		close(pipex->pipe_fd[0]);
+		close(pipex->pipe_fd[1]);
+		execve(pipex->cmds[0][0], pipex->cmds[0], envp);
+		ft_putstr_fd("\033[0;33mcommand not found: ", 2);
+		ft_putstr_fd(pipex->cmds[0][0], 2);
+		ft_putstr_fd("\033[0m\n", 2);
+		exit(127);
+	}
+	pid2 = fork();
+	if (pid2 < 0)
+	{
+		ft_putstr_fd("Error al crear el proceso hijo\n", 2);
+		exit(1);
+	}
+	if (pid2 == 0)
+	{
+		dup2(pipex->pipe_fd[0], 0);
+		dup2(outfile_fd, 1);
+		close(pipex->pipe_fd[0]);
+		close(pipex->pipe_fd[1]);
+		close(outfile_fd);
+		execve(pipex->cmds[1][0], pipex->cmds[1], envp);
+		ft_putstr_fd("\033[0;33mcommand not found: ", 2);
+		ft_putstr_fd(pipex->cmds[1][0], 2);
+		ft_putstr_fd("\033[0m\n", 2);
+		exit(127);
+	}
+	close(pipex->pipe_fd[0]);
+	close(pipex->pipe_fd[1]);
+	close(infile_fd);
+	close(outfile_fd);
+	waitpid(pid1, &last_exit_cmd_status, 0);
+	waitpid(pid2, &last_exit_cmd_status, 0);
 	if (WIFEXITED(last_exit_cmd_status))
 	{
 		ft_alloc_lst(0, 0);
